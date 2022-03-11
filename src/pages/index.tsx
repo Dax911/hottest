6f06a9f3a01291e9bd4ac271b875e1b7493494eb
Dax911 <haydenaylor@icloud.com>
2022-03-11 17:19:13 -0500

    spacing change

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -35,7 +35,7 @@ const [first, second] = getNFTsForVote();
 
   return (
     <div className="h-screen w-screen flex flex-col justify-center items-center">
-      <div className="text-2xl text-center">Hot or Not? <br />Which NFT is better?</div>
+      <div className="text-2xl text-center">Hot or Not?<br />Which NFT is better?</div>
       <div className="p-2" />
       <div className="border rounded p-8 flex justify-between items-center max-w-2xl">
         <div className="w-16 h-16 bg-red-800">{first}</div>
