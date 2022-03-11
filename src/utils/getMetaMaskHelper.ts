ab593358d8accfe08899fdb7dee1f8e46d5feac5
Dax911 <haydenaylor@icloud.com>
2022-03-11 16:08:58 -0500

    commented out ether MetaMaskHelper for prod

--- src/utils/getMetaMaskHelper.ts
+++ src/utils/getMetaMaskHelper.ts
@@ -1,20 +1,20 @@
-import { ethers } from 'ethers';
+//import { ethers } from 'ethers';
 
-export const connect = () => {
-  return new Promise(async (resolve, reject) => {
-    try {
-      const provider = new ethers.providers.Web3Provider(window.ethereum)
-      await provider.send("eth_requestAccounts", [])
-      const signer = provider.getSigner()
-      const accountAddress = await signer.getAddress()
-
-      document.getElementById("current_address").innerText = accountAddress
-      document.getElementById("current_address").removeAttribute("hidden")
-
-      resolve(accountAddress)
-    } catch (error) {
-      console.error(error)
-      reject(error)
-    }
-  })
-}
+//export const connect = () => {
+//  return new Promise(async (resolve, reject) => {
+//    try {
+//      const provider = new ethers.providers.Web3Provider(window.ethereum)
+//      await provider.send("eth_requestAccounts", [])
+//      const signer = provider.getSigner()
+//      const accountAddress = await signer.getAddress()
+//
+//      document.getElementById("current_address").innerText = accountAddress
+//      document.getElementById("current_address").removeAttribute("hidden")
+//
+//      resolve(accountAddress)
+//    } catch (error) {
+//      console.error(error)
+//      reject(error)
+//    }
+//  })
+//}
