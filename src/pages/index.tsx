d827a71e57296c0f36d2850bd996e413fed10820
Dax911 <haydenaylor@icloud.com>
2022-03-21 09:40:12 -0500

    re added package json and eslint disabled

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+/* eslint-disable jsx-a11y/alt-text */
+/* eslint-disable @next/next/no-img-element */
 /* eslint-disable react/jsx-no-comment-textnodes */
 import { trpc } from '@/utils/trpc'
 import { OnboardingButton } from '@/utils/getMetaMaskHelper'
@@ -8,52 +10,23 @@ const btn =
   'inline-flex items-center px-3 py-1.5 border border-gray-300 shadow-sm font-medium rounded-full text-gray-700 bg-white hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500'
 
 export default function Home() {
-  //const {isConnected, isInstalled } = method
-
-  //if isInstalled == false
-  //  return install staleTime
-
-  //if isInstalled == true && isConnected == false or error
-  //return state for not connected (SignIN)
-
-  //while isInstalled == true && isConnected == true
-  //  return game state and retrieve the assets
-  //  game loop here
-
-  //get assets from wallet send contract addresses for the NFT or ipfs location to server for store
-  //run store compare save only diffs
-  //get random number that is the index of the range of the database and then calls that address for display on the page
-  //gets winner and increments the winner counter for it and sends response back.
-
-  //need to sign in w MetaMask
-  //connect and read NFTs from wallet
-  //compare the NFTs in cleint wallet to DB if not in DB add it
-  //randomly select two NFTs from database and comparegetMetaMaskHelper
-  //display
-  //take vote send answer request to DB
-
-  // Using HTTPS
-
-  //should i just have an entry feild for wallet addresses? and do it from that?
+  
   const accounts = React.useState([] as any).toString();
 
   const [ids, updateIds] = useState(() => getNFTsForVote())
   const [first, second] = ids
   
 
-  const voteForHottest = () => {
-    //todo: fire mutation to persist vote
-  }
+  
   const pairNFTs = trpc.useQuery(['get-NFT-pair'])
   const secondNFT = trpc.useQuery(['get-NFT-pair'])
 
   if (pairNFTs.isLoading && accounts.length < 0) {
-    return <p>Loading...</p>
+    return(<p>Loading...</p>)
   } else {
     console.log(pairNFTs)
     console.log(accounts)
 
-    //console.log(firstNFT.data[2].address)
     const voteMutation = trpc.useMutation(["cast-vote"]);
 
     const voteForHottest = (selected: number) => {
@@ -65,9 +38,10 @@ export default function Home() {
       // todo: fire mutation to persist changes
       updateIds(getNFTsForVote());
     };
-    const firstNFTimage = pairNFTs.data?.firstNft.imageUrl || 'https://picsum.photos/200/300'
-    const secondNFTimage = pairNFTs.data?.secondNft.imageUrl || 'https://picsum.photos/200/300'
-    //iterate over the NFTs and display them
+
+    const firstNFTimage = pairNFTs.data?.firstNft.imageUrl || 'https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png?w=640'
+    const secondNFTimage = pairNFTs.data?.secondNft.imageUrl || 'https://socialistmodernism.com/wp-content/uploads/2017/07/placeholder-image.png?w=640'
+
     return (
  <div className="flex flex-col items-center justify-center w-screen h-screen">
         <div className="text-2xl text-center">
