fc4a662dc4095494e4ec717db86395b581e67a3b
Dax911 <haydenaylor@icloud.com>
2022-04-11 17:24:59 -0500

    changed url

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -29,8 +29,7 @@ export default withTRPC<AppRouter>({
      * @link https://trpc.io/docs/ssr
      */
     const url = process.env.VERCEL_URL
-      ? `https://${process.env.VERCEL_URL}/api/trpc`
-      : "http://localhost:3000/api/trpc";
+      ? 'https://hottestnfts.vercel.app/api/trpc';
 
     return {
       header() {
