cba3809d0cf42fa599afa78154d0c2943f02e9b8
Dax911 <haydenaylor@icloud.com>
2022-04-11 10:15:26 -0500

    100

--- src/utils/getRandomIndex.ts
+++ src/utils/getRandomIndex.ts
@@ -1,10 +1,11 @@
 import { prisma } from "../backend/utils/prisma";
 
 
+
 export const getNFTlocation: (notThisOne?: number) => number = (
   notThisOne
 ) => {
-  const MAX_INDEX_SIZE = 100 //TODO: make this dynamic
+  const MAX_INDEX_SIZE =  100 //TODO: make this dynamic from the database size
 
   const randNFTindexNumberforLocation = Math.floor(Math.random()*MAX_INDEX_SIZE);
 
