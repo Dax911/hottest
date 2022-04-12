25e58a1fa0722aba9bf5d04658718bd4da9b4f97
Dax911 <haydenaylor@icloud.com>
2022-04-12 10:57:28 -0500

    removed optional

--- src/backend/router/index.ts
+++ src/backend/router/index.ts
@@ -77,7 +77,7 @@ export const appRouter = trpc.router().query( "get-NFT-pair", {
       const nfts = await ( Web3api.alchemy.getNfts( { owner: accounts } ) )
       const nullVal = null
 
-      const formattedNfts = nfts.ownedNfts?.filter( 
+      const formattedNfts = nfts.ownedNfts.filter( 
         ( nft: NftMetadata ) => 
         nft.id.tokenMetadata.tokenType === "ERC721" && 
         nft.metadata.image?.startsWith( "https" ) && 
